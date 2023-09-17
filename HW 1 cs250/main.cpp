0edbee65699a75d319dcbbce9213a45c0fd129c3
bita-yonas <bitaniayonasyonas@gmail.com>
2023-09-17 15:31:45 -0400

    Initial Commit

---
+++ HW 1 cs250/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW 1 cs250
+//
+//  Created by bita yonas on 9/17/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
